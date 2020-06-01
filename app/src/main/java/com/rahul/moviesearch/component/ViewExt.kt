5fc1cbfb8c2d48ff8d40dd6baff8c9dfe25bde33
Rahul ch <59815914+Rahul-xfers@users.noreply.github.com>
2020-06-01 14:58:35 +0800

    initial setup

---
+++ app/src/main/java/com/rahul/moviesearch/component/ViewExt.kt
@@ -0,0 +1,30 @@
+package com.rahul.moviesearch.component
+
+import android.view.View
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.databinding.BindingAdapter
+
+/**
+ * View extension class any modifications to any class that extends
+ * view will be handled here
+ *
+ *
+ */
+
+/**
+ * Make View visible and Gone
+ * @param visible  when true it is visble else its gone
+ */
+@BindingAdapter("makeVisible")
+fun View.makeVisible(visible: Boolean) {
+    this.visibility = if (visible) View.VISIBLE else View.GONE
+}
+
+/**
+ * Make TextView visible only when text is not empty
+ * @param visible if text is empty the view will be made gone else visible
+ */
+@BindingAdapter("visibleOnText")
+fun AppCompatTextView.visibleOnText(visible: String?) {
+    this.visibility = if (visible.isNullOrEmpty()) View.GONE else View.VISIBLE
+}
